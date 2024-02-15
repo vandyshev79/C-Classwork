85f180fb8ab39185735fbc376fc3583f3bb9578c
Alexey Vandyshev <vandyshev79@gmail.com>
2024-02-16 00:25:49 +0500

    add folder lesson1

---
+++ lesson1/Task5/Program.cs
@@ -0,0 +1,10 @@
+﻿int n = 5;
+int i = 1;
+
+while (i <= n)
+{
+    //Console.WriteLine(i);
+    Console.Write(i);
+    Console.Write(' ');
+    i = i + 1;
+}
---
+++ lesson1/homework1/Program.cs
@@ -0,0 +1,7 @@
+﻿int a = 2;
+int b = 15;
+int c = 8;
+int d = 8;
+double sum = c + d;
+double quotient = (a * b) / sum;
+Console.WriteLine(quotient);
