adcd7a54528abf89dd0b3f5b224497506ae5fa14
Alexey Vandyshev <vandyshev79@gmail.com>
2024-02-19 20:03:05 +0500

    add Task2 in to Seminar1

---
+++ seminar1/Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход принимает целое число N, а на выходе 
+// показывает все ЦЕЛЫЕ числа в промежутке от -N до N.
+
+Console.Write("Введите число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+if (number1 < 0)
+{
+    number1 = - number1;
+}
+int number2 = - number1;
+while (number2 <= number1)
+{
+    Console.Write($"{number2} ");
+    number2++;  
+}
+// number2 = number2 + 1; 
+    //number--; number = number - 1;
+    // number += 10; number = number + 10;
