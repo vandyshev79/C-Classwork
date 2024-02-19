b2baeb001f471284927c1dc77a2433502678e59b
Alexey Vandyshev <vandyshev79@gmail.com>
2024-02-19 23:56:35 +0500

    add avtoproverka_Task3

---
+++ avtoproverka/Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Проверка на чётность.
+
+Console.Write("Введите число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+if (number1 % 2 == 0)
+{
+    Console.WriteLine("Чётное");
+}
+else
+{
+    Console.WriteLine("Нечётное");
+}
