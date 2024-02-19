7398f68a93fdd6caaec58a404eec8d704bd8a459
Alexey Vandyshev <vandyshev79@gmail.com>
2024-02-19 23:11:05 +0500

    add Taskk3_version2

---
+++ seminar1/Task3_version2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программму, которая принимает на вход трёхзначное целое число
+// и на выходе показывает сумму первой и последней цифры этого числа. МЕТОД СТРОК!!!
+
+Console.WriteLine("Введите трехзначное число: ");
+string a = Console.ReadLine()!;
+if (a.Length == 3)
+{
+    Console.WriteLine(Convert.ToInt32(Convert.ToString(a[0])) +
+                    Convert.ToInt32(Convert.ToString(a[2])));
+}
+else
+{
+    Console.WriteLine("Число не трёхзначное");
+}
+//  
