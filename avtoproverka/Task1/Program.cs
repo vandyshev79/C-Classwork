e1d8c5a52d370b9b8718c32b1e6614d262bfebc6
Alexey Vandyshev <vandyshev79@gmail.com>
2024-02-20 00:52:55 +0500

    add avtoproverka Task1

---
+++ avtoproverka/Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Внутри класса Answer напишите метод CompareNumbers, который на вход принимает два числа и выводит, какое число большее, а какое меньшее.
+Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());        
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number2 > number1)
+{
+    Console.WriteLine("{1} большее {0} меньшее", number1, number2);
+}
+else
+{
+    Console.WriteLine("{0} большее {1} меньшее", number1, number2);
+}
