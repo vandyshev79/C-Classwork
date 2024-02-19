c9f4f092580f24153e15023a39b6f63b3354f8b6
Alexey Vandyshev <vandyshev79@gmail.com>
2024-02-20 01:19:34 +0500

    change Task1 avtoproverka

--- avtoproverka/Task1/Program.cs
+++ avtoproverka/Task1/Program.cs
@@ -5,9 +5,13 @@ Console.Write("Введите второе число: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
 if (number2 > number1)
 {
-    Console.WriteLine("{1} большее {0} меньшее", number1, number2);
+    Console.WriteLine("второе число больше");
 }
-else
+if (number2 == number1)
 {
-    Console.WriteLine("{0} большее {1} меньшее", number1, number2);
+    Console.WriteLine("Числа равны");
+}
+if (number1 > number2)
+{
+     Console.WriteLine("первое число больше");
 }
