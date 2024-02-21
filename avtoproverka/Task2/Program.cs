330922f52d3a39ef8a4814921645cb4b7b092d59
Alexey Vandyshev <vandyshev79@gmail.com>
2024-02-21 23:18:53 +0500

    add avtoproverka.Task2

---
+++ avtoproverka/Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Максимальное из трех чисел. Hапишите метод, 
+// который принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Write("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int thirdNumber = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+
+if(firstNumber > max)
+{
+    max = firstNumber;
+}
+if(secondNumber > max)
+{
+    max = secondNumber;
+}
+if(thirdNumber > max)
+{
+    max = thirdNumber;
+}
+
+Console.WriteLine("max = " + max);   
