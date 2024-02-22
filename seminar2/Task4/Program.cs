01bc3e333f6080ebdc1c74c62b2699b2dc1086bd
Alexey Vandyshev <vandyshev79@gmail.com>
2024-02-22 19:40:49 +0500

    add Task4 in to seminar2

---
+++ seminar2/Task4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая выводит третью слева цифру заданного числа
+// или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    while (num > 999)
+    {
+        num /= 10; // num = num / 10
+    }
+    Console.WriteLine(num % 10);
+}
