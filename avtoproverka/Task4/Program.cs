26427cf3a80b0c1373c5fe70fe9115696b36f138
Alexey Vandyshev <vandyshev79@gmail.com>
2024-02-23 00:31:42 +0500

    add Task4 in to avtoproverka

--- avtoproverka/Task4/Program.cs
+++ avtoproverka/Task4/Program.cs
@@ -1,4 +1,7 @@
-﻿// Вывести четные числа
+﻿// Напишите программу, которая на вход принимает число (number), 
+// а на выходе выводит все чётные числа от 1 до number (включительно), 
+// после каждого числа должен быть знак пробела.
+// Вывести четные числа
 
 Console.Write("Введите число больше нуля: ");
 int number = Convert.ToInt32(Console.ReadLine());
@@ -6,7 +9,7 @@ if (number <= 0)
 {
     Console.WriteLine("Число неправильное");    
 }
-int number1 = number % 2;
+int number1 = 2;
 while (number1 <= number)
 {
     if (number1 % 2 == 0)
