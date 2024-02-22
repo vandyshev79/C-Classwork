3c8c583b7137eb4b0e30dfaf7258845fd532b4e7
Alexey Vandyshev <vandyshev79@gmail.com>
2024-02-22 18:40:02 +0500

    add Task2 in to seminar2

---
+++ seminar2/Task2/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и 
+// возводит вторую цифру этого числа в степень, равную третьей цифре.
+
+Console.WriteLine("Введите трёхзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int num1 = num / 10 % 10;
+int num2 = num % 10;
+Console.WriteLine(Math.Pow(num1,num2));
