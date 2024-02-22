af103f88b166db699a5de69a5094dffc80eeda35
Alexey Vandyshev <vandyshev79@gmail.com>
2024-02-23 03:40:01 +0500

    add Task2 and Task3 in to Homework

---
+++ Homework/Task2/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая принимает на вход координаты точки (X и Y), 
+// причём X ≠ 0 и Y ≠ 0 и выдаёт номер координатной четверти плоскости, 
+// в которой находится эта точка.
+
+Console.WriteLine("Введите координату Х: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x == 0 && y == 0)
+{
+    Console.WriteLine("Неправильные координаты");
+}
+else
+{
+        if (x >= 0 && y >= 0)
+    {
+        Console.WriteLine("1");
+    } 
+    if (x < 0 && y >= 0)
+    {
+        Console.WriteLine("2");
+    }
+    if (x < 0 && y < 0)
+    {
+        Console.WriteLine("3");
+    }
+    if (x >= 0 && y < 0)
+    {
+        Console.WriteLine("4");
+    }
+}
---
+++ Homework/Task3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает
+// на вход целое число из отрезка [10, 99] и показывает
+// наибольшую цифру числа.
+
+Console.WriteLine("Введите число в диапазоне от 10 до 99: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 10 || num >= 99)
+{
+    Console.WriteLine($"Неправильное число!!!");
+}
+else
+{
+int num1 = num / 10;
+int num2 = num % 10;
+
+if (num1 > num2)
+{
+    Console.WriteLine(num1);
+}
+else Console.WriteLine(num2);
+}
+// Console.WriteLine(num1);
+// Console.WriteLine(num2);
