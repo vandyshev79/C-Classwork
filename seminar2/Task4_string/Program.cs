6c49d2733a600b5b6a157e85381ce2efab4e4017
Alexey Vandyshev <vandyshev79@gmail.com>
2024-02-22 19:46:34 +0500

    add Task4_string in to seminar2

---
+++ seminar2/Task4_string/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая выводит третью слева цифру заданного числа
+// или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+string num_str = Convert.ToString(num);
+if (num_str.Length < 3)
+{
+Console.WriteLine("Третьей цифры нет");
+}
+else Console.WriteLine(num_str[2]);
