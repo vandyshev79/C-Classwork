a81e059ba5a46d1e35cbc517deb1726bfb8ce041
Alexey Vandyshev <vandyshev79@gmail.com>
2024-02-22 21:12:07 +0500

    add Task4 in to avtoproverka

---
+++ avtoproverka/Task4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Вывести четные числа
+
+Console.Write("Введите число больше нуля: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number <= 0)
+{
+    Console.WriteLine("Число неправильное");    
+}
+int number1 = number % 2;
+while (number1 <= number)
+{
+    if (number1 % 2 == 0)
+    {
+        Console.Write($"{number1} ");
+    }
+    number1++;  
+}
