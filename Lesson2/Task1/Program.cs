7a79b762c496e00334276545c710097ea027da0a
Alexey Vandyshev <vandyshev79@gmail.com>
2024-02-24 14:04:23 +0500

    add Lesson2:Task1-3

---
+++ Lesson2/Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿// int[] arr = new int[5];
+// arr[0] = 5;
+// arr[1] = 7;
+// arr[2] = 5;
+// arr[3] = 4;
+// arr[4] = 3;
+// Console.WriteLine(arr[0]);
+// Console.WriteLine(arr[1]);
+// Console.WriteLine(arr[2]);
+// Console.WriteLine(arr[3]);
+// Console.WriteLine(arr[4]);
+
+// int[] arr2 = new int[5] { 1, 2, 3, 4, 5 };
+
+// Создать массив и заполнить его целыми числами от 1 до N (N - произвольное натуральное число).
+// Вывести элементы массива на экран
+
+int n = 10;
+int[] arr = new int[n];
+int i = 0;
+
+while (i < arr.Length)
+{
+    arr[i] = i + 1;
+    Console.Write($"{arr[i]} ");
+    i++;
+}
---
+++ Lesson2/Task3/Program.cs
@@ -0,0 +1,34 @@
+﻿int n = 5;
+int[] arr = { 2, 4, 7, 3, 9 };
+int i = 0;
+int max = arr[0];
+
+while (i < n)
+{
+    if (arr[i] > max)
+    {
+        max = arr[i];
+    }
+    i = i + 1;
+}
+Console.WriteLine(max);
+
+max = arr[0];
+for (int j = 0; j < n; j++)
+{
+    if (arr[j] > max)
+    {
+        max = arr[j];
+    }   
+} 
+Console.WriteLine(max);
+
+max = arr[0];
+foreach(int e in arr)
+{
+   if (e > max)
+    {
+        max = e;
+    }    
+}
+Console.WriteLine(max);
