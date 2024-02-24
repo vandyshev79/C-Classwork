c8c898e833bb320fa28c3e3dfae56124b45cd301
Alexey Vandyshev <vandyshev79@gmail.com>
2024-02-24 16:04:59 +0500

    add changes in Task4:Homework

--- Homework/Task4/Program.cs
+++ Homework/Task4/Program.cs
@@ -2,20 +2,43 @@
 // принимает натуральное число N, а на выходе
 // показывает его цифры через запятую.
 
-Console.WriteLine("Введите число: ");
-string num = Console.ReadLine()!;
+// Console.WriteLine("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
 
-if (num.Length == 1)
-{
-    Console.WriteLine(num);
-}
-else
-{
-    int l = num.Length;
-    int i = 0;
-    while (i < num.Lenght)
+// if (num < 10)
+// {
+//     Console.WriteLine(num);
+// }
+// else
+// {
+//     while (num > 0)
+//     {
+//         int currentDigit = num % 10;
+//         num /= 10;
+//         if (num > 0)
+//         {
+//         Console.Write(currentDigit + ",");
+//         }
+//         else
+//         {
+//         Console.WriteLine(currentDigit);
+//         }
+// }
+// }
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int len_n = Convert.ToInt32(Convert.ToString(n).Length);
+
+while (len_n > 0) 
+{    
+    int z = n / Convert.ToInt32((Math.Pow(10,len_n - 1))) % 10;
+    if (len_n > 0)
     {
-        
+         Console.Write(z + ",");    
     }
-    Console.WriteLine(l);
+    else Console.Write(z);
+    // {
+    //     Console.Write(z);   
+    // }
+    len_n--;
 }
