95c184bf122a2c003388037ba48c6d8b7c392668
Alexey Vandyshev <vandyshev79@gmail.com>
2024-02-26 08:58:29 +0500

    add changes in Homework:Task4

--- Homework/Task4/Program.cs
+++ Homework/Task4/Program.cs
@@ -2,43 +2,34 @@
 // принимает натуральное число N, а на выходе
 // показывает его цифры через запятую.
 
-// Console.WriteLine("Введите число: ");
-// int num = Convert.ToInt32(Console.ReadLine());
-
-// if (num < 10)
-// {
-//     Console.WriteLine(num);
-// }
-// else
-// {
-//     while (num > 0)
-//     {
-//         int currentDigit = num % 10;
-//         num /= 10;
-//         if (num > 0)
-//         {
-//         Console.Write(currentDigit + ",");
-//         }
-//         else
-//         {
-//         Console.WriteLine(currentDigit);
-//         }
-// }
-// }
 Console.WriteLine("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
 int len_n = Convert.ToInt32(Convert.ToString(n).Length);
+//Console.WriteLine($"{len_n} - количество цифр в числе");
 
-while (len_n > 0) 
-{    
-    int z = n / Convert.ToInt32((Math.Pow(10,len_n - 1))) % 10;
+while (len_n > 0)
+{
+    int a = n % 10;
+    int currDigit = n / Convert.ToInt32((Math.Pow(10, len_n - 1))) % 10;
     if (len_n > 0)
     {
-         Console.Write(z + ",");    
+        Console.Write(currDigit + ",");
     }
-    else Console.Write(z);
+    else
+        if (currDigit == a)
+    {
+        Console.Write(currDigit);
+    }
+    // if (currDigit < a)
     // {
-    //     Console.Write(z);   
+    //     Console.Write(currDigit + ",");
     // }
+    // else Console.Write(currDigit);
     len_n--;
 }
+
+
+
+// int n = 246145869;
+// n = n % 10;
+// Console.WriteLine(n);
