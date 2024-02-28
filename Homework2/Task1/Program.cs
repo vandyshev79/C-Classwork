775917c1a27845f861b6bf5cbd0f20111f4c174c
Alexey Vandyshev <vandyshev79@gmail.com>
2024-02-28 19:50:30 +0500

    Создал Homework:Task1,2,3

---
+++ Homework2/Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая бесконечно
+// запрашивает целые числа с консоли. Программа
+// завершается при вводе символа ‘q’ или при вводе
+// числа, сумма цифр которого четная.
+
+// Пример
+// 5 12 16 q [STOP]
+// 3 45 342 15 [STOP]
+
+Console.Clear();
+// string Numbers (string num);
+// {
+//    for (int i = 0; i <= num.Length; i++)
+//     {
+//         string p = Math.Pow(10, symbol.Length - i - 1);
+//     }
+//     return num;
+// }
+
+
+Console.WriteLine("Введите число или символ: ");
+string num = Console.ReadLine();
+Console.WriteLine();
