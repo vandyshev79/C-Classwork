7bec4ebc7672a3de3bd490c415d0c4eaf4cf7570
Alexey Vandyshev <vandyshev79@gmail.com>
2024-02-29 00:39:16 +0500

    add lesson4:Task2

--- lesson4/Task1/Program.cs
+++ lesson4/Task1/Program.cs
@@ -42,13 +42,13 @@ int[,] CreateMatrix(int rowCount, int columsCount)
 void ShowMatrix(int[,] matrix)
 {
     for (int i = 0; i < matrix.GetLength(0); i++)
-{
-    for (int j = 0; j < matrix.GetLength(1); j++)
     {
-        Console.Write($"{matrix[i, j]} ");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
     }
-    Console.WriteLine();
-}
 }
 
 
