a6098fa3284e85ac2494bc67a26f3590bcfdef55
Alexey Vandyshev <vandyshev79@gmail.com>
2024-02-28 23:05:44 +0500

    Добавил вывод двумерного массива в lesson4:Task1

--- lesson4/Task1/Program.cs
+++ lesson4/Task1/Program.cs
@@ -1,1 +1,58 @@
-﻿
+﻿// Вывод двумерного массива на экран(консоль).
+
+
+
+// int[,] matrix = new int[3, 5];
+// Random rnd = new Random();
+// for (int i = 0; i < matrix.GetLength(0); i++)
+// {
+//     for (int j = 0; j < matrix.GetLength(1); j++)
+//     {
+//         matrix[i, j] = rnd.Next(1, 11);
+//     }
+// }
+
+// for (int i = 0; i < matrix.GetLength(0); i++)
+// {
+//     for (int j = 0; j < matrix.GetLength(1); j++)
+//     {
+//         Console.Write($"{matrix[i, j]} ");
+//     }
+//     Console.WriteLine();
+// }
+
+
+
+
+int[,] CreateMatrix(int rowCount, int columsCount)
+{
+    int[,] matrix = new int[rowCount, columsCount];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 11);
+        }
+    }
+    return matrix;
+}
+
+
+void ShowMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        Console.Write($"{matrix[i, j]} ");
+    }
+    Console.WriteLine();
+}
+}
+
+
+Console.Clear();
+int[,] matrix = CreateMatrix(4, 5);
+ShowMatrix(matrix);
+
